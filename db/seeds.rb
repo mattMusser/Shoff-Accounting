d6445bea2d377378a1f1f98d8cb83e3ca46c7948
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-02-21 16:03:35 -0600

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create Clients
+10.times do
+	user = User.find_or_initialize_by(client_name: Faker::Company.name, email: Faker::Internet.safe_email) do |user|
+		user.password = Faker::Internet.password
+	end
+	user.skip_confirmation!
+	user.save!
+end
+
+# Create Admin User
+admin = User.find_or_inititalize_by(client_name: 'admin', email: 'markshare101@gmail.com') do |user|
+	user.password = 'password'
+end
+admin.skip_confirmation!
+admin.save!
+
+# Create Standard User
+standard = User.find_or_initialize_by(client_name: 'standard', email: 'markshare101@gmail.com') do |user|
+	user.password = 'password'
+end
+standard.skip_confirmation!
+standard.save!
+users = User.all
+
+puts "Seeding Finished"
+puts "-.-.-.-.-.-.-.-.-"
+puts "#{User.count} total users"
