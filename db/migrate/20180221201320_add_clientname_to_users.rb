3f36de5b333cbcb8cfe2c587555ace089fafbd33
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-02-21 14:25:41 -0600

    change clientname to client_name

--- db/migrate/20180221201320_add_clientname_to_users.rb
+++ db/migrate/20180221201320_add_clientname_to_users.rb
@@ -1,5 +1,5 @@
 class AddClientnameToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :clientname, :string
+    add_column :users, :client_name, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20180221201320) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "clientname"
+    t.string "client_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
