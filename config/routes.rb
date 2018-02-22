9898ed68637baa00ed302c402754092a7029dc7f
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-02-22 09:09:11 -0600

    generate cancancan ability file and users controller/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
  
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/update'
+
   devise_for :users
   devise_scope :user do
     root to: "devise/sessions#new"
