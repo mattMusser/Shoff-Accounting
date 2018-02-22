3a0110dfc40de2c98d74d3118a58558c1979fca8
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-02-22 09:27:53 -0600

    rename users controller to clients and configure clients controller

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,24 @@
+class ClientsController < ApplicationController
+  def index
+    @clients = User.all
+  end
+
+  def show
+    @clients = User.all
+  end
+
+  def update
+    if current_user.update_attributes(client_params)
+      flash[:notice] = "Client information was updated."
+      redirect_to current_user
+    else
+      flash[:error] = "An error occured. Please try again."
+      redirect_to edit_user_registration_path
+    end
+  end
+
+  private
+  def client_params
+    params.require(:user).permit(:client_name, :email)
+  end
+end
