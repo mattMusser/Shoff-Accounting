6266974b4382024c6c8961c1f950289e4b17b1a4
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-02-22 20:24:58 -0600

    user story complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
  
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/update'
-
   devise_for :users
   devise_scope :user do
     root to: "devise/sessions#new"
   end
+
+  resources :users
+  root "welcome#faq"
 end
