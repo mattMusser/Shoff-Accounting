dd38b4088fe7b63e5ce0dd161628b21d2a078457
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-02-26 13:38:54 -0600

    generate welcome controller again

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,4 @@
+class WelcomeController < ApplicationController
+  def faq
+  end
+end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get welcome_index_url
-    assert_response :success
-  end
-
   test "should get faq" do
     get welcome_faq_url
     assert_response :success
