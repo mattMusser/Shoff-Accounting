8af4182127d64980224f5228c217687f60c671c5
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-03-01 12:48:14 -0600

    embed WIP

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   devise_scope :user do
-    #root to: "devise/sessions#new"
     delete "logout" => "devise/sessions#destroy", :as => "logout"
   end
 
   resources :users
-
+  
   get 'users', to: "users#index", as: "clients"
   get 'users/:id', to: "users#show", as: "client"
+  get 'welcome', to: "welcome#faq"
 
   root 'users#show'
 end
