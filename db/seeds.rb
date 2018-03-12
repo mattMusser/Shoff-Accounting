4529d1dbb35bf090051075bbdd937163900dd073
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-03-12 16:28:50 -0500

    rewrite code for link creation in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,12 +29,31 @@ admin.save!
 users = User.all
 
 # Create Google Sheet Links
-5.times do
-	link = Link.create!(
-		url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQBGkvCVm8armqckARgkdgRPC7BT49eFcnIhwrz6zYVcIWmDC7QpfpRTfjaRdpTE5DYRnOdFhmQtAPT/pubhtml",
-		user: users.sample
-	)
-end
+
+link_one = Link.create!(
+	url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTiC8xOjR4iwQp7Zu0Hq-Net9zUYdHh2W92NxJc5QZabiRLhSqY6fbg6z__yXlJn0eRSfMUXlxMYRKn/pubhtml",
+	user: users.find_by(client_name: 'Creative Photography')
+)
+link_one.save!
+
+link_two = Link.create!(
+	url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ10cC-dSxIbU7Q0XcdFopfCvRKglYhqFKQY8PfzVC6W5Wa2uItlJclMiW7JsbmfnkAs4FMWjlHLTFe/pubhtml",
+	user: users.find_by(client_name: 'Photography Company')
+)
+link_two.save!
+
+link_three = Link.create!(
+	url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSzLniL8xYG3-veqge3dxY8vrp-5CfwVvAz0kjdNIzrxvq6QZ6NyhjTfPAqOwznNe9G5eIgkB1x0YoS/pubhtml",
+	user: users.find_by(client_name: 'Web Photos')
+)
+link_three.save!
+
+link_four = Link.create!(
+	url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRolYBhyrmvXnCgnsEw4ZR1QMyhvLarxscGH9o9VXnqt-RDVNsqfsD6KREv_YisghD6xuow_QSzxjvg/pubhtml",
+	user: users.find_by(client_name: 'Samson Creative')
+)
+link_four.save!
+
 links = Link.all
 
 puts "Seeding Finished"
